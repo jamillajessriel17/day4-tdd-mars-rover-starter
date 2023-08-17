f403174c18d7ca02f8099e3552d809e35c801a41
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-17 19:58:02 +0800

    test: create test for turning right(SOUTH) when the initial direction is EAST

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -144,4 +144,18 @@ class MarsRoverTest {
         Assertions.assertEquals(0 , currentLocation.getY());
         Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_direction_0_0_S_when_executeCommand_given_0_0_E_and_command_turn_right() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.EAST);
+        Command givenCommand = Command.TURN_RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        Assertions.assertEquals(0 , currentLocation.getX());
+        Assertions.assertEquals(0 , currentLocation.getY());
+        Assertions.assertEquals(Direction.SOUTH , currentLocation.getDirection());
+    }
 }
