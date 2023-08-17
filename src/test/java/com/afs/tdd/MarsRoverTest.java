791560a6198e1691dbd25b97fad17f6118303e8e
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-17 19:00:26 +0800

    test: create test for turning left

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -60,4 +60,18 @@ class MarsRoverTest {
         Assertions.assertEquals(0 , currentLocation.getY());
         Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_direction_0_0_W_when_executeCommand_given_0_0_N_and_command_turn_left() {
+        // Given
+        Location initialLocation = new Location(0, 0, Direction.NORTH);
+        Command givenCommand = Command.TURN_LEFT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        // When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        // Then
+        Assertions.assertEquals(0 , currentLocation.getX());
+        Assertions.assertEquals(0 , currentLocation.getY());
+        Assertions.assertEquals(Direction.WEST , currentLocation.getDirection());
+    }
 }
